3c52c64341e9b9c4747230290bb972984e42ab16
Sean Cashin <scashin133@gmail.com>
2010-09-27 22:44:29 -0700

    version bump for apostrophe fix.

--- lib/net/ldap.rb
+++ lib/net/ldap.rb
@@ -246,7 +246,7 @@ require 'net/ldap/entry'
 # and then keeps it open while it executes a user-supplied block.
 # Net::LDAP#open closes the connection on completion of the block.
 class Net::LDAP
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 
   class LdapError < StandardError; end
 
