516569a3e702701815dbf114857163fe7a3ca6ad
Sean Cashin <scashin133@gmail.com>
2013-10-17 15:01:42 -0700

    removed test that was causing infinite loops. added test for making sure that wrap_with_ssl accepts the ssl_version option

--- spec/unit/ldap_spec.rb
+++ spec/unit/ldap_spec.rb
@@ -7,11 +7,11 @@ describe Net::LDAP::Connection do
         flexmock(TCPSocket).
           should_receive(:new).and_raise(Errno::ECONNREFUSED)
       end
-      
+
       it "should raise LdapError" do
         lambda {
           Net::LDAP::Connection.new(
-            :server => 'test.mocked.com', 
+            :server => 'test.mocked.com',
             :port   => 636)
         }.should raise_error(Net::LDAP::LdapError)
       end
@@ -21,11 +21,11 @@ describe Net::LDAP::Connection do
         flexmock(TCPSocket).
           should_receive(:new).and_raise(SocketError)
       end
-      
+
       it "should raise LdapError" do
         lambda {
           Net::LDAP::Connection.new(
-            :server => 'test.mocked.com', 
+            :server => 'test.mocked.com',
             :port   => 636)
         }.should raise_error(Net::LDAP::LdapError)
       end
@@ -35,14 +35,35 @@ describe Net::LDAP::Connection do
         flexmock(TCPSocket).
           should_receive(:new).and_raise(NameError)
       end
-      
+
       it "should rethrow the exception" do
         lambda {
           Net::LDAP::Connection.new(
-            :server => 'test.mocked.com', 
+            :server => 'test.mocked.com',
             :port   => 636)
         }.should raise_error(NameError)
       end
+    end
+  end
+  describe "wrap_with_ssl" do
+    before(:each) do
+        @conn = flexmock(TCPSocket).should_receive(:new).and_return(flexmock('tcpsocket'))
+        @ssl_con = flexmock('sslsocket')
+        @ssl_con.should_receive(:connect)
+        @ssl_con.should_receive(:sync_close=)
+        flexmock(OpenSSL::SSL::SSLSocket).should_receive(:new).and_return(@ssl_con)
+      end
+    it "allows no options to be passed" do
+      flexmock(OpenSSL::SSL::SSLContext).new_instances.should_receive(:ssl_version=).never
+      Net::LDAP::Connection.wrap_with_ssl(@conn).should eql(@ssl_con)
+    end
+    it "allows options to be passed" do
+      flexmock(OpenSSL::SSL::SSLContext).new_instances.should_receive(:ssl_version=).never
+      Net::LDAP::Connection.wrap_with_ssl(@conn, {}).should eql(@ssl_con)
+    end
+    it "communicates over the ssl_version configured" do
+      flexmock(OpenSSL::SSL::SSLContext).new_instances.should_receive(:ssl_version=).with(:SSLv3)
+      Net::LDAP::Connection.wrap_with_ssl(@conn, {:ssl_version => :SSLv3}).should eql(@ssl_con)
     end
   end
 end
